0978b33bb4f446542f9d31eea82eb9c09b2eefc7
Diego Varela <djvarela89@gmail.com>
2022-05-18 14:06:10 -0300

    agregando scrips para comparar letras dentro de la palabra secreta

--- assets/js/ahorcado.js
+++ assets/js/ahorcado.js
@@ -4,11 +4,10 @@ let botonNuevoJuego = document.querySelector("#btn-nuevoJ")
 let estado = document.getElementById("ganaPierde");
 let palabraPropuesta="";
 document.getElementById("ingresarLetra").focus()
-let palabraSecreta=["perro", "gato", "ocelote", "aguila"];
+let palabraSecreta=["perrito", "gatito", "ocelote", "aguila"];
 
 let errores = 0;
 let aciertos = 0;
-let esta = false;
 
 botonNuevoJuego.onclick=verificar;
 
@@ -16,7 +15,20 @@ botonNuevoJuego.onclick=verificar;
 function iniciar(){
     errores = 0;
     aciertos = 0;
-   
+    dibujarPrincipal()
+    var canvas = document.getElementById('canvas');
+    if (canvas.getContext) {
+      var ctx = canvas.getContext('2d');
+      ctx.beginPath();
+      ctx.fillStyle = "#fff"
+      ctx.moveTo(100,10); 
+      ctx.lineTo(100,400);
+      ctx.moveTo(100,11);
+      ctx.lineTo(201,11);
+      ctx.moveTo(200,10); 
+      ctx.lineTo(200,30);
+      ctx.stroke();
+    }
     let lineas = document.querySelector(".lineas");
 
     lineas.innerHTML=""
@@ -29,50 +41,39 @@ function iniciar(){
         lineas.appendChild(span);
    }
 
-
+  
 }
 
 iniciar()
 
 
-function verificar(){
-    spans = document.querySelectorAll(".lineas span")
-    let letraIngresada= document.getElementById("ingresarLetra").value;
 
-    for(let i = 0; i < palabraPropuesta.length; i++){
-        if(letraIngresada == palabraPropuesta[i]){
+function verificar(){
+    const spans = document.querySelectorAll(".lineas span")
+    let letraIngresada= document.getElementById("ingresarLetra").value;
+    let esta = false;
+
+        for(let i = 0; i < palabraPropuesta.length; i++){
+            if(letraIngresada == palabraPropuesta[i]){
             spans[i].innerHTML= letraIngresada
             esta = true;
             aciertos++
       
+           }
         }
-     }
-     if(esta == false){
+      if(esta == false){
         errores++;
-        // console.log("errores")
-     }
-     if(errores == palabraPropuesta.length){
+        dibujarPrincipal()
+         }
+      if(errores == 6){
         ganaPierde.innerHTML= "Usted Perdio";
-     }
+      }
+      if(aciertos == palabraPropuesta.length){
+        ganaPierde.innerHTML= "Usted gano";
+      }
     // console.log("la letra " + letraIngresada+ "en la plabara "+ palabraPropuesta + "existe? "+ esta)
     document.getElementById("ingresarLetra").value="";
     document.getElementById("ingresarLetra").focus()
 }
 
-
-
-
-function dibujar() {
-    var canvas = document.getElementById('canvas');
-    if (canvas.getContext) {
-      var ctx = canvas.getContext('2d');
-      ctx.beginPath();
-    ctx.moveTo(10,50);
-    ctx.lineTo(100,75);
-    
-    ctx.closePath();
-    ctx.fill();
-    }
-  }
-
-  dibujar()
+// dibujarPrincipal()
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,5 +2,54 @@
 function elegirPalabra(){
     let palabraElegida = Math.floor(Math.random()*palabraSecreta.length - 0) + 0;  
     return palabraElegida;
+}
+
+function dibujarPrincipal() {
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+    /// dibujar cara
+    if(errores == 1){
+      ctx.beginPath();
+      ctx.arc(200,40,10,0,Math.PI*2,true); // Círculo externo
+      ctx.stroke();
+      
+    }if(errores == 2){
+    //cuerpo
+
+        ctx.beginPath();
+        ctx.moveTo(200,50)
+        ctx.lineTo(200, 90)
+        ctx.stroke();
+    }if(errores == 3){
+        //brazos der
+        ctx.beginPath();
+        ctx.moveTo(200,50)
+        ctx.lineTo(180, 80)
+        ctx.stroke();
+    }if(errores == 4){
+        //brazos izq
+        ctx.beginPath();
+        ctx.moveTo(200,50)
+        ctx.lineTo(220, 80)
+        ctx.stroke();
+    }if(errores == 5){
+            // patas izq
+        ctx.beginPath();
+        ctx.moveTo(200,90)
+        ctx.lineTo(170, 120)
+        ctx.stroke();
+    }if(errores == 6){
+        // patas izq
+        ctx.beginPath();
+        ctx.moveTo(200,90)
+        ctx.lineTo(230, 120)
+        ctx.stroke();
     }
+  
     
+    }
+}
+  
+
+
+
