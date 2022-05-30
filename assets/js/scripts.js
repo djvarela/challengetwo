6a5cd012edb9a0fa6e619e1142351d65cfc1654d
Diego Varela <djvarela89@gmail.com>
2022-05-30 15:57:13 -0300

    cargando funciones del juego

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,91 +1,133 @@
+const btnJuegoNuevo = document.getElementById("nuevo-juego")
 
-function elegirPalabra(){
-    let palabraElegida = Math.floor(Math.random()*palabraSecreta.length - 0) + 0;  
-    return palabraElegida;
+function selector( str ){
+    return document.getElementById( str );
 }
 
-function dibujarPrincipal(errores) {
-    var ctx = canvas.getContext('2d');
-   
+function selectorPalabra( nmin, nmax ){
+    const valores = nmax - nmin; 
+    const palabraAleatoria = Math.floor( Math.random( ) * valores ) + nmin; 
+    return palabraAleatoria;
+}
+
+
+
+function dibujarHorca(errores) {
+    var pincel = canvas.getContext("2d");
+    
     switch(errores){
-    case 1:{
+        case 1:{
          /// dibujar cara
-        ctx.beginPath();
-        ctx.arc(200,40,10,0,Math.PI*2,true); 
-        ctx.stroke();
+        pincel.beginPath();
+        pincel.arc(200,40,10,0,Math.PI*2,true); 
+        pincel.stroke();
         break;}
     case 2:{
     //cuerpo
-        ctx.beginPath();
-        ctx.moveTo(200,50)
-        ctx.lineTo(200, 90)
-        ctx.stroke();
+        pincel.beginPath();
+        pincel.moveTo(200,50)
+        pincel.lineTo(200, 90)
+        pincel.stroke();
     break;}
     case 3:{
     //brazos der
-        ctx.beginPath();
-        ctx.moveTo(200,50)
-        ctx.lineTo(180, 80)
-        ctx.stroke();
+        pincel.beginPath();
+        pincel.moveTo(200,50)
+        pincel.lineTo(180, 80)
+        pincel.stroke();
     break;}
     case 4:{
     //brazos izq
-        ctx.beginPath();
-        ctx.moveTo(200,50)
-        ctx.lineTo(220, 80)
-        ctx.stroke();
+        pincel.beginPath();
+        pincel.moveTo(200,50)
+        pincel.lineTo(220, 80)
+        pincel.stroke();
     break;}
     case 5:{
     // patas izq
-        ctx.beginPath();
-        ctx.moveTo(200,90)
-        ctx.lineTo(170, 120)
-        ctx.stroke();
+        pincel.beginPath();
+        pincel.moveTo(200,90)
+        pincel.lineTo(170, 120)
+        pincel.stroke();
     break;}
     case 6:{
     // patas izq
-        ctx.beginPath();
-        ctx.moveTo(200,90)
-        ctx.lineTo(230, 120)
-        ctx.stroke();
+        pincel.beginPath();
+        pincel.moveTo(200,90)
+        pincel.lineTo(230, 120)
+        pincel.stroke();
     break;}
-    default: return;
+   
     }
     
 }
-  
 
 
-
-function letraError(letraIngresada){
-    
-    let letraEquivocada= document.getElementById("letrasErradas")
-    // let p =document.createElement("p")
-  
-    letraErradas = letraIngresada + letraErradas;
-  
-    letraEquivocada.innerHTML=letraErradas;
+function dibujar(){
 
 
+    errores = 0;
+    aciertos = 0;
+          
+    var canvas = document.getElementById("canvas");
+    if (canvas.getContext) {
+    var pincel = canvas.getContext("2d");
+    pincel.strokeStyle = "slateblue";
+    pincel.lineWidth = 1.5;
+    pincel.beginPath();
+    pincel.moveTo(100,10); 
+    pincel.lineTo(100,400);
+    pincel.moveTo(100,11);
+    pincel.lineTo(201,11);
+    pincel.moveTo(200,10); 
+    pincel.lineTo(200,30);     
+    pincel.stroke();
+        }
+     
+     
 }
-
-
-
 
 
 
 function desistir(){
     location.reload();
-    areaJuego.style.display ="none";
-   
-  }
-
-
-function nJuego(){
-    esta = false
-    errores = 0;
-    aciertos = 0;
-    jugar()
-    elegirPalabra();
-    input.style.display=""
 }
+
+
+
+function nuevojuego(){
+    var pincel = canvas.getContext("2d");
+    pincel.clearRect(0, 0, pincel.canvas.clientWidth, pincel.canvas.clientHeight);
+  
+    errores=0;
+    aciertos = 0;
+    acerto = false;
+    letraErradas= [];
+    iniciar();
+    
+    dibujarHorca(errores)
+    selector("resultado").innerHTML =""
+}
+
+
+function agregarPalabra(){
+    nuevaPalabra.style.display="";
+    btnJugar.style.display="none"
+    btnAgregarPalabra.style.display="none"
+    textAgregarPalabra.style.display="";
+}
+
+function guardarPalabraNueva(){
+    let palabraNueva= textAgregarPalabra.value;
+    if(palabraNueva == ""){
+        alert("debe ingresar una palabra")
+    } else{
+        palabras.push(palabraNueva)
+        console.log(palabras)
+    }
+    nuevaPalabra.style.display="none"
+    nuevojuego()
+}
+
+
+btnJuegoNuevo.onclick=nuevojuego
